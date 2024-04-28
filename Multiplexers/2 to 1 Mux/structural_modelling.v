19678e44fb441ccca2ebe2e39d95ade6a51aeb10
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-04-28 14:20:44 +0530

    Structural Modelling for 2:1 Mux

---
+++ Multiplexers/2 to 1 Mux/structural_modelling.v
@@ -0,0 +1,10 @@
+//Structural Modelling for 2:1 Mux
+
+module mux_2_to_1_str(Y, I0, I1, S);
+output Y; input I0, I1; input S;
+wire y1, y2, y3;
+not g1(y1, S);
+and g2(y2, y1, I0);
+and g3(y3, S, I1);
+or g4(Y, y2, y3);
+endmodule
