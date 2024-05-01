c3836cb69b469e0523b7594f1a69fced7a855e96
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-05-01 15:19:26 +0530

    Test Bench for 1:2 DeMux

--- Demultiplexers/1 to 2 DeMux/dataflow_modelling.v
+++ Demultiplexers/1 to 2 DeMux/dataflow_modelling.v
@@ -1,6 +1,6 @@
 //Dataflow Modelling for 1:2 DeMux
 
-module deMux_1_to_2(I, S, Y0, Y1);
+module deMux_1_to_2_df(I, S, Y0, Y1);
 input I; input S; output Y0, Y1;
 assign Y0 = I&~S;
 assign Y1 = I&S;
